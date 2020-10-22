07e3832e9f03380b4001a8cf33b3f6265ebe2364
= <=>
2020-10-22 11:46:43 +0100

    layout

---
+++ main.js
@@ -0,0 +1,6 @@
+const collider = require('./classes/collider');
+const collider = require('./classes/controller');
+const collider = require('./classes/game');
+const collider = require('./classes/player');
+const collider = require('./classes/screen');
+const collider = require('./classes/tile');
