523bab7ec69e7fc6555c0d42dffbece02259755e
= <=>
2020-10-22 16:05:12 +0100

    Completed player, screen, main, tile

---
+++ classes/controller.js
@@ -0,0 +1,17 @@
+class Controller {
+  constructor() {
+    this.player = new Player();
+  }
+
+  movePlayer(key) {
+    if (key === leftKey) {
+      this.player.setCurrentLocation(-x, 0);
+    } else if (key === rightKey) {
+      this.player.setCurrentLocation(x, 0);
+    } else if (key === upKey) {
+      this.player.setCurrentLocation(0, -y);
+    } else if (key === downKey) {
+      this.player.setCurrentLocation(0, y);
+    }
+  }
+}
--- classes/player.js
+++ classes/player.js
@@ -1,18 +1,19 @@
 class Player {
-    constructor() {
-        this.isAlive = true;
-        this.currentXLocation = x;
-        this.currentYLocation = y;
-    }
-    setCurrentLocation([x, y]) {
-        //shorthand
-        this.currentXLocation += x
-        //does same thing
-        this.currentYLocation = this.currentYLocation + y
-    }
-    drawSelf() {
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.fillRect = (this.currentXLocation, this.currentYLocation, height, width);
-    }
+  constructor() {
+    this.isAlive = true;
+    this.currentXLocation = x;
+    this.currentYLocation = y;
+  }
+  setCurrentLocation(x, y) {
+    //shorthand
+    this.currentXLocation += x;
+    //does same thing
+    this.currentYLocation = this.currentYLocation + y;
+  }
+  drawSelf() {
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.fillRect =
+      (this.currentXLocation, this.currentYLocation, height, width);
+  }
 }
