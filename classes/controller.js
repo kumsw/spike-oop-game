e6639f7c11596298ea3f59e3890227cd4002df24
= <=>
2020-10-23 13:33:28 +0100

    started working on jump

--- classes/controller.js
+++ classes/controller.js
@@ -3,15 +3,16 @@ class Controller {
     this.player = new Player();
   }
 
-  movePlayer(keyCode) {
+  async movePlayer(keyCode) {
     if (keyCode === 37 || keyCode === 65) {
       this.player.setCurrentLocation(-unit, 0);
     } else if (keyCode === 39 || keyCode === 68) {
       this.player.setCurrentLocation(unit, 0);
     } else if (keyCode === 38 || keyCode === 87) {
       this.player.setCurrentLocation(0, -unit);
+      await setTimeout(() => { this.player.setCurrentLocation(0, unit) }, 1000)
     } else if (keyCode === 40 || keyCode === 83) {
-      this.player.setCurrentLocation(0, unit);
+      this.player.setCurrentLocation(0, unit)
     }
   }
   drawPlayer() {
