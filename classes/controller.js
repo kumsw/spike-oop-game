d352d4fe1b4b7387847525b05026dc932631948f
= <=>
2020-10-23 14:54:45 +0100

    stuff

--- classes/controller.js
+++ classes/controller.js
@@ -5,9 +5,13 @@ class Controller {
 
   movePlayer(keyCode) {
     if (keyCode === 37 || keyCode === 65) {
-      this.player.setCurrentLocation(-unit, 0);
+      if (this.player.currentXLocation > 0.1) {
+        this.player.setCurrentLocation(-unit, 0);
+      }
     } else if (keyCode === 39 || keyCode === 68) {
-      this.player.setCurrentLocation(unit, 0);
+      if (this.player.currentXLocation < 19 * unit) {
+        this.player.setCurrentLocation(unit, 0);
+      }
     } else if (keyCode === 38 || keyCode === 87) {
       if (this.player.currentYLocation === 8 * unit) {
         this.player.setCurrentLocation(0, -unit);
